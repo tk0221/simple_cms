34ade59523f015afd766ccff4c35515ad4b887bf
Taehun Kim <kim.taehun0221@gmail.com>
2021-01-12 00:33:45 -0800

    ch3-2

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -4,4 +4,9 @@ class DemoController < ApplicationController
 
   def index
   end
+
+  def hello
+    # render('index')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'demo#index'
   get 'demo/index'
+  get 'demo/hello'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
