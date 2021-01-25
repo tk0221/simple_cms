364b29d048205d138def7fc81a6bc95184609bb3
Taehun Kim <kim.taehun0221@gmail.com>
2021-01-24 18:08:47 -0800

    CURD

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,33 @@
 Rails.application.routes.draw do
 
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
-  get 'pages/edit'
-  get 'pages/delete'
-  get 'subjects/index'
-  get 'subjects/show'
-  get 'subjects/new'
-  get 'subjects/edit'
-  get 'subjects/delete'
+
   root 'demo#index'
   get 'demo/index'
   get 'demo/hello'
   get 'demo/contact'
   get 'demo/about'
+
+  resources :subjects do
+    member do
+      get :delete
+    end
+  end
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
+  # get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/new'
+  # get 'pages/edit'
+  # get 'pages/delete'
+  # get 'subjects/index'
+  # get 'subjects/show'
+  # get 'subjects/new'
+  # get 'subjects/edit'
+  # get 'subjects/delete'
+
 
 
   #default routes
